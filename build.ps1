b37f2440e2f8ad02cf656a7621d69d15befef689
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-10-07 20:51:16 -0400

    fixes

--- build.ps1
+++ build.ps1
@@ -2,24 +2,6 @@
 param(
     # Build task(s) to execute
     [parameter(ParameterSetName = 'task', position = 0)]
-    [ArgumentCompleter( {
-        param($Command, $Parameter, $WordToComplete, $CommandAst, $FakeBoundParams)
-        $psakeFile = './psakeFile.ps1'
-        switch ($Parameter) {
-            'Task' {
-                if ([string]::IsNullOrEmpty($WordToComplete)) {
-                    Get-PSakeScriptTasks -buildFile $psakeFile | Select-Object -ExpandProperty Name
-                }
-                else {
-                    Get-PSakeScriptTasks -buildFile $psakeFile |
-                        Where-Object { $_.Name -match $WordToComplete } |
-                        Select-Object -ExpandProperty Name
-                }
-            }
-            Default {
-            }
-        }
-    })]
     [string[]]$Task = 'default',
 
     # Bootstrap dependencies
@@ -29,11 +11,7 @@ param(
     [parameter(ParameterSetName = 'Help')]
     [switch]$Help,
 
-    # Optional properties to pass to psake
-    [hashtable]$Properties,
-
-    # Optional parameters to pass to psake
-    [hashtable]$Parameters
+    [pscredential]$PSGalleryApiKey
 )
 
 $ErrorActionPreference = 'Stop'
@@ -42,24 +20,23 @@ $ErrorActionPreference = 'Stop'
 if ($Bootstrap.IsPresent) {
     Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-    if ((Test-Path -Path ./requirements.psd1)) {
-        if (-not (Get-Module -Name PSDepend -ListAvailable)) {
-            Install-Module -Name PSDepend -Repository PSGallery -Scope CurrentUser -Force
-        }
-        Import-Module -Name PSDepend -Verbose:$false
-        Invoke-PSDepend -Path './requirements.psd1' -Install -Import -Force -WarningAction SilentlyContinue
-    } else {
-        Write-Warning 'No [requirements.psd1] found. Skipping build dependency installation.'
+    if (-not (Get-Module -Name PSDepend -ListAvailable)) {
+        Install-Module -Name PSDepend -Repository PSGallery -Scope CurrentUser
     }
+    Invoke-PSDepend -Path './requirements.psd1' -Install -Import -Force -WarningAction SilentlyContinue
 }
 
 # Execute psake task(s)
 $psakeFile = './psakeFile.ps1'
 if ($PSCmdlet.ParameterSetName -eq 'Help') {
-    Get-PSakeScriptTasks -buildFile $psakeFile |
+    Get-PSakeScriptTasks -buildFile $psakeFile  |
         Format-Table -Property Name, Description, Alias, DependsOn
 } else {
     Set-BuildEnvironment -Force
-    Invoke-psake -buildFile $psakeFile -taskList $Task -nologo -properties $Properties -parameters $Parameters
-    exit ([int](-not $psake.build_success))
+    $parameters = @{}
+    if ($PSGalleryApiKey) {
+        $parameters['galleryApiKey'] = $PSGalleryApiKey
+    }
+    Invoke-psake -buildFile $psakeFile -taskList $Task -nologo -parameters $parameters
+    exit ( [int]( -not $psake.build_success ) )
 }
--- psakeFile.ps1
+++ psakeFile.ps1
@@ -1,14 +1,43 @@
 properties {
-    # Set this to $true to create a module with a monolithic PSM1
-    $PSBPreference.Build.CompileModule = $false
-    $PSBPreference.Help.DefaultLocale = 'en-US'
-    $PSBPreference.Test.OutputFile = 'out/testResults.xml'
 
-    if ($PSGalleryApiKey) {
+    $PSBPreference.Build.CompileModule   = $true
+    $PSBPreference.Build.CopyDirectories = @('SQL', 'en-US')
+    $PSBPreference.Build.CompileHeader   = @'
+using namespace System.Management.Automation
+using namespace System.Collections.ObjectModel
+'@
+    $PSBPreference.Help.DefaultLocale = 'en-US'
+
+    # Test settings
+    $PSBPreference.Test.OutputFile = 'out/testResults.xml'
+    $PSBPreference.Test.ImportModule                =   $true
+    $PSBPreference.Test.OutputFile                  = [IO.Path]::Combine($PSBPreference.Build.OutDir, 'testResults.xml')
+    $PSBPreference.Test.ScriptAnalysis.SettingsPath = [IO.Path]::Combine($PSBPreference.Test.RootDir, 'ScriptAnalyzerSettings.psd1')
+
+    # Publish settings
+    if ($galleryApiKey) {
         $PSBPreference.Publish.PSRepositoryApiKey = $galleryApiKey.GetNetworkCredential().password
     }
 }
 
 task Default -depends Test
 
-task Test -FromModule PowerShellBuild -minimumVersion '0.6.1'
+task Pester -FromModule PowerShellBuild -minimumVersion '0.6.1' -preaction { Remove-Module PSEdgeKeywords -ErrorAction SilentlyContinue }
+
+task InstallAct {
+    if (-not (Get-Command -Name act -CommandType Application -ErrorAction SilentlyContinue)) {
+        if ($IsWindows) {
+            choco install act-cli
+        } elseif ($IsLinux) {
+            curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+        } elseIf ($IsMacOS) {
+            brew install nektos/tap/act
+        }
+    } else {
+        'act already installed'
+    }
+}
+
+task TestGHAction -depends Build, InstallAct  {
+    act -j test -P ubuntu-latest=nektos/act-environments-ubuntu:18.04
+}
